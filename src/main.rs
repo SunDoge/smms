00be3af3c6d1f6180a7e511740ac3f13c7850f26
SunDoge <384813529@qq.com>
2018-10-26 22:24:45 +0800

    clap

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,25 @@ use clap::{Arg, App};
 use glob::glob;
 
 fn main() {
-    let matches = App::new("smms cli").version(crate_version!()).author(crate_authors!()).about("do something").arg(Arg::with_name("filename").long("file").short("f")).arg(Arg::with_name("delete").long("delete").short("d")).get_matches();
+    let matches = App::new("smms cli")
+    .version(crate_version!())
+    .author(crate_authors!())
+    .about("do something")
+    .arg(Arg::with_name("file").long("file").short("f").takes_value(true))
+    .arg(Arg::with_name("delete").long("delete").short("d").takes_value(true))
+    .arg(Arg::with_name("list").long("list").short("l"))
+    .arg(Arg::with_name("history").long("history"))
+    .arg(Arg::with_name("clear").long("clear"))
+    .arg(Arg::with_name("filename").takes_value(true))
+    .get_matches();
 
-    tokio::run_async(async {
-        await!(history()).unwrap()
+    tokio::run_async(async move {
+        if matches.is_present("history") {
+            await!(history()).unwrap();
+        }
     });
 }
+
+// async fn run() {
+//     await!(history()).unwrap();
+// }
