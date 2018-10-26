9f5912b0f7359500da203ac4d603a57804c2daf2
SunDoge <384813529@qq.com>
2018-10-26 19:22:06 +0800

    reuse client

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #[macro_use]
 extern crate tokio;
 
+#[macro_use]
+extern crate serde_derive;
+
 pub mod api;
 
 use reqwest::r#async::{Client, Decoder};
@@ -10,9 +13,31 @@ use std::io::{self, Cursor};
 use std::mem;
 use tokio::prelude::*;
 
-// pub fn upload(filename: &str) {} -> impl Future<Item=(), Error=()> {
-//     Client::new().post("")
-// }
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    width: u32,
+    height: u32,
+    filename: String,
+    storename: String,
+    size: usize,
+    path: String,
+    hash: String,
+    timestamp: usize,
+    url: String,
+    delete: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct History {
+    code: String,
+    data: Vec<Data>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Message {
+    code: String,
+    msg: String,
+}
 
 pub async fn history() -> Result<(), ()> {
     // await!(Client::new()
@@ -43,4 +68,34 @@ pub async fn history() -> Result<(), ()> {
     });
 
     Ok(())
+}
+
+pub async fn upload(filename: &str) -> Result<(), ()> {
+    unimplemented!()
+}
+
+pub struct SmmsClient {
+    client: Client,
+}
+
+impl SmmsClient {
+    pub fn new() -> SmmsClient {
+        SmmsClient {
+            client: Client::new(),
+        }
+    }
+
+    pub async fn list(&self) -> Result<(), ()> {
+        let mut res = await!(self.client.get(api::HISTORY).send())
+        .map_err(|err| println!("request error: {}", err))?;
+        println!("{}", res.status());
+        let body = mem::replace(res.body_mut(), Decoder::empty());
+        let body = await!(body.concat2()).map_err(|err| println!("concat2 error: {}", err))?;
+        let mut body = Cursor::new(body);
+        let _ = io::copy(&mut body, &mut io::stdout()).map_err(|err| {
+            println!("stdout error: {}", err);
+        });
+
+        Ok(())
+    }
 }
